0dcf970511a52bceab14f3115eb5cd559a8392c6
Anand Jha <anandkumarj669@gmail.com>
2025-05-10 12:36:18 +0530

    Added another allowed origin for Sarang

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // CORS configuration
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", "http://localhost:7000", "https://myshopcluess.web.app"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", "http://localhost:7000", "https://myshopcluess.web.app", "https://flipkart-webapp-964d9.web.app"];
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin) || !origin) {
